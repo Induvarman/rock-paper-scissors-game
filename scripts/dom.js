f85b8a977ff3ddb77aa58aca26e23042095970a1
Induvarman <induvarman8@gmail.com>
2026-06-24 09:55:08 +0530

    Finished writing functions such as PlayGame and its helper functions. Set up the dom.js file and ensured everything working

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,7 +1,15 @@
 const buttons = document.querySelectorAll("button");
+const playerScore = document.querySelector(".player-score");
+const computerScore = document.querySelector(".computer-score");
+const champ = document.querySelector(".champ");
+const round = document.querySelector(".round");
+
+let humanScore = 0;
+let botScore = 0;
+let roundNum = 0; 
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    
+    playGame(e);
   })
 })
--- scripts/game.js
+++ scripts/game.js
@@ -2,80 +2,57 @@ function getComputerChoice() {
   const choices = ['rock', 'paper', 'scissor'];
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
-
-  // let randomNumber = Math.random();
-  // let computerResult = "";
-
-  // if (randomNumber >= 0 && randomNumber < 1 / 3)
-  //   computerResult = "rock";
-  // else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3)
-  //   computerResult = "paper";
-  // else if (randomNumber >= 2 / 3 && randomNumber < 1)
-  //   computerResult = "scissor";
-
 }
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
     return "tie";
   }
-  else if (humanChoice === "rock" && computerChoice === "scissor") || 
+  else if ((humanChoice === "rock" && computerChoice === "scissor") || 
       (humanChoice === "paper" && computerChoice === "rock") ||
-      (humanChoice === "scissor" && computerChoice === "paper") {
-    humanScore++;
+      (humanChoice === "scissor" && computerChoice === "paper")) {
+    return "player";
   }
   else {
-    computerScore++;
+    return "computer";
   }
-
-  // if (humanChoice === "rock" && computerChoice === "scissor") {
-  //   console.log("You Win! Rock beats Scissor.");
-  //   humanScore++;
-  // }
-  // else if (humanChoice === "rock" && computerChoice === "paper") {
-  //   console.log("You Lose! Paper beats rock.");
-  //   computerScore++;
-  // }
-  // else if (humanChoice === "paper" && computerChoice === "rock") {
-  //   console.log("You Win! Paper beats Rock.");
-  //   humanScore++;
-  // }
-  // else if (humanChoice === "paper" && computerChoice === "scissor") {
-  //   console.log("You Lose! Scissor beats paper.");
-  //   computerScore++;
-  // }
-  // else if (humanChoice === "scissor" && computerChoice === "rock") {
-  //   console.log("You Lose! Rock beats Scissor.");
-  //   computerScore++;
-  // }
-  // else if (humanChoice === "scissor" && computerChoice === "paper") {
-  //   console.log("You Win! Scissor beats Paper.");
-  //   humanScore++;
-  // }
-  // else {
-  //   console.log("Tie!");
-  // }
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame(e) {
+  const humanChoice = e.target.className;
+  const computerChoice = getComputerChoice();
 
-function playGame() {
-  let humanSelection = "";
-  let computerSelection = "";
-  for (let i = 0; i < 5; i++) {
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
+  const roundResult = playRound(humanChoice, computerChoice);
+  round.textContent = `Round ${++roundNum}`
+
+  if (roundResult === "player") {
+  playerScore.textContent = ++humanScore;
   }
-  console.log(`Win : ${humanScore} Lose : ${computerScore}`);
-  if (humanScore > computerScore) 
-    console.log("You Win the Series!");
-  else if (humanScore < computerScore) 
-    console.log("Computer Win the Series!")
-  else 
-    console.log("The Series is Tie!");
+  else if (roundResult === "computer") {
+  computerScore.textContent = ++botScore;
+  }
+
+  const isGameOver = (humanScore === 5 || botScore === 5);
+
+  if (isGameOver) {
+    declareChamp();
+    resetGame();
+  }
+  else {
+  champ.textContent = "⁉️";
+  }
 }
 
-// playGame();
+function declareChamp() {
+  const matchWinner = (humanScore > botScore)? "You" : "Computer";
+  champ.textContent = matchWinner;
+}
 
+function resetGame() {
+  humanScore = 0;
+  botScore = 0;
+  roundNum = 0;
+  playerScore.textContent = humanScore;
+  computerScore.textContent = botScore;
+  round.textContent = `Play Again`;
+}
